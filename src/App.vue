38a1d26726d64a385dddc13368758485aab7baa7
Gregor Tokarev <zippper123915@gmail.com>
2022-01-06 16:58:58 +0300

    Fix error with email auth

--- src/App.vue
+++ src/App.vue
@@ -36,14 +36,13 @@ onMounted(async () => {
 router.beforeEach((to, from, next) => {
   if (isInAppRoute(to)) {
     const isAuthorized = store.getters['authModule/isAuthorized']
-    console.log(isAuthorized)
 
     if (!isAuthorized) {
-      next({ name: 'auth-login' })
-    } else {
-      next()
+      return next({ name: 'auth-login' })
     }
-    next()
+    return next()
   }
+
+  next()
 })
 </script>
